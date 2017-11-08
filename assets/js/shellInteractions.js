f6ed83c5a7609d3df1d119d2bce1b81e7bd34247
Kai <krausekai@users.noreply.github.com>
2017-11-08 21:42:19 +1100

    Fixed Longman and Kotobank search, fixed css

--- assets/js/shellInteractions.js
+++ assets/js/shellInteractions.js
@@ -10,24 +10,16 @@ var doc = document;
 
 //Disable evals for scripts
 window.eval = global.eval = function() {
-	throw new Error("Sorry, we do not support window.eval() for security reasons.");
+	throw new Error("window.eval() blocked");
 }
 
 //Disable drag drop events
 doc.addEventListener('dragover', function (e) {e.preventDefault()});
 doc.addEventListener('drop', function (e) {e.preventDefault()});
 
-// pagination check
+// pagination function setup
 var page = window.location.href;
 if (page.endsWith("index.html")) {
-	index();
-} else {
-	window.addEventListener('click', defaultClickback, false);
-	window.addEventListener('auxclick', defaultClickback, false);
-}
-
-// for the page named "index.html"
-function index () {
 	//Assign an audio player (currently only for ldoce/longman dictionary)
 	var globalAudio = doc.getElementById('globalAudioPlayer');
 	function playAudio(e) {
@@ -40,46 +32,73 @@ function index () {
 	}
 	doc.addEventListener('click', playAudio, true);
 
-	// From the tabOne (dictionary) div, input clicked URL's inner website URL text as terms into the searchbar
-	function tabOneClickback(e) {
-		var e = window.e || e;
+	// Dictionary tab
+	var tabOne = doc.getElementById("tabOne");
+	tabOne.addEventListener('click', tabOneClickback, false);
+	tabOne.addEventListener('auxclick', defaultClickback, false);
+	// Corpora tab
+	var tabTwo = doc.getElementById("tabTwo");
+	tabTwo.addEventListener('click', defaultClickback, false);
+	tabTwo.addEventListener('auxclick', defaultClickback, false);
+} else {
+	window.addEventListener('click', defaultClickback, false);
+	window.addEventListener('auxclick', defaultClickback, false);
+}
 
-		//return if right click
-		if (e.which == 3) {
-			return;
+// for the page named "index.html"
+function tabOneClickback(e) {
+	var e = window.e || e;
+
+	//return if right click
+	if (e.which == 3) {
+		return;
+	}
+
+	// if this is a clicked URL
+	if (e.target.localName == 'a' || e.target.localName == 'A' || e.target.parentNode.nodeName == 'a' || e.target.parentNode.nodeName == 'A') {
+		e.preventDefault();
+
+		var url = e.target.parentNode.href || e.target.href;
+
+		// Fix file:\\ paths
+		var target = e.target || e.srcElement;
+		var targetClass = target.className;
+
+		// Do a top parent check to break look check
+		while(target){
+			if (targetClass == "ldoceDictOutput" || targetClass == "kotobankDict") {
+				break;
+			}
+			target = target.parentNode;
+			targetClass = target.className;
 		}
 
-		// if this is a clicked URL
-		if (e.target.localName == 'a' || e.target.localName == 'A' || e.target.parentNode.nodeName == 'a' || e.target.parentNode.nodeName == 'A') {
-			e.preventDefault();
-			// For dictionary results, load other results in like iframes for that dictionary only
-			var url = e.target.parentNode.href || e.target.href;
-			if (url.startsWith("http://ejje.weblio.jp/content/") || url.startsWith("https://ejje.weblio.jp/content/")) {
-				weblioDictionary(url);
-				return;
+		if (url.startsWith("file:")) {
+			if (targetClass == "ldoceDictOutput" && url.includes("/dictionary/")) {
+				var searchTerm = url.match("dictionary.*");
+				url = "https://www.ldoceonline.com/" + searchTerm;
 			}
-			if (url.startsWith("http://kotobank.jp/word/") || url.startsWith("https://kotobank.jp/word/")) {
-				kotobankDictionary(url);
-				return;
+			else if (targetClass == "kotobankDict" && url.includes("/word/")) {
+				var searchTerm = url.match("word.*");
+				url = "https://kotobank.jp/" + searchTerm;
 			}
-			if (url.startsWith("http://www.ldoceonline.com/dictionary/") || url.startsWith("https://www.ldoceonline.com/dictionary/") || url.startsWith("http://www.ldoceonline.com/search/direct/") ||  url.startsWith("https://www.ldoceonline.com/search/direct/")) {
-				ldoceDictionary(url);
-				return;
-			}
-			// Else, open default
-			//defaultClickback(e);
+		}
+
+		if (url.startsWith("http://ejje.weblio.jp/content/") || url.startsWith("https://ejje.weblio.jp/content/")) {
+			weblioDictionary(url);
+		}
+		else if (url.startsWith("http://kotobank.jp/word/") || url.startsWith("https://kotobank.jp/word/")) {
+			kotobankDictionary(url);
+		}
+		else if (url.startsWith("http://www.ldoceonline.com/dictionary/") || url.startsWith("https://www.ldoceonline.com/dictionary/") || url.startsWith("http://www.ldoceonline.com/search/direct/") ||  url.startsWith("https://www.ldoceonline.com/search/direct/")) {
+			ldoceDictionary(url);
+		}
+		else {
 			// Else, input the clicked URL text into the input box for searching
 			var input = doc.getElementById("search").elements["searchbar"];
 			input.value = e.target.innerText;
 		}
 	}
-	// Dictionary tab
-	var tabOne = doc.getElementById("tabOne");
-	tabOne.addEventListener('click', tabOneClickback, false);
-	// Corpora tab
-	var tabTwo = doc.getElementById("tabTwo");
-	tabTwo.addEventListener('click', defaultClickback, false);
-	tabTwo.addEventListener('auxclick', defaultClickback, false);
 }
 
 // Open other URLs with default programs and not Electron
@@ -91,7 +110,7 @@ function defaultClickback(e) {
 		return;
 	}
 
-	if (e.target.localName == 'a') {
+	if (e.target.localName == 'a' || e.target.localName == 'A' || e.target.parentNode.nodeName == 'a' || e.target.parentNode.nodeName == 'A') {
 		e.preventDefault();
 		shell.openExternal(e.target.href);
 	}
