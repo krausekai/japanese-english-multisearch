518cd214570be98c1776dd480814d1c1de700882
krausekai <25758458+krausekai@users.noreply.github.com>
2018-06-06 01:29:30 +1000

    remove feature creep, update packages

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,20 +4,6 @@ const searchHistory = require("./js/searchHistory");
 const webScraper = require("./js/webscraper");
 const shellinteractions = require("./js/shellInteractions");
 
-// RakutenMA Tokenizer
-var rakuten;
-const RakutenMA = require("rakutenma");
-const fs = require("fs");
-const path = require("path");
-const rakutenModelPath = path.join(__dirname, "./js/rakutenma/model_ja.min.json");
-fs.readFile(rakutenModelPath, "utf8", (err, data) => {
-	let model = JSON.parse(data);
-	rakuten = new RakutenMA(model);
-	rakuten.featset = RakutenMA.default_featset_ja;
-	rakuten.hash_func = RakutenMA.create_hash_func(15);
-});
-
-
 // app interaction
 var doc = document;
 var lastSearchTime = Date.now();
@@ -38,21 +24,6 @@ function searchBar() {
 
 	// Get search type
 	var searchType = doc.getElementById("search").elements["searchType"].value;
-
-	// Tokenize sentences if there are multiple terms, from the dictionary tab
-	var terms = rakuten.tokenize(searchTerm);
-	var glossTokens = document.getElementById("glossTokens");
-	if (terms.length > 1 && searchType == "dictionary") {
-		// show visibility
-		glossTokens.innerHTML = "";
-		glossTokens.style.visibility = "visible";
-		for (var i = 0; i < terms.length; i++) {
-			// Remove term types (eg. noun, verb)
-			terms[i] = terms[i][0];
-			// tie this into shell interactions as a safe search term
-			glossTokens.innerHTML += "<a class='token' href='#glossToken'>" + terms[i] + "</a>";
-		}
-	}
 
 	//Reset the scrollbar
 	var tableBodies = doc.getElementsByClassName("tbodySize");
